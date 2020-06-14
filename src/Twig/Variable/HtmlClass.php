05d9b1eb1962b0a3723b4b94241446af5e0446ac
nanawel <nanawel@gmail.com>
2020-06-14 09:28:48 +0200

    Fix scroll behavior (smooth on forms, regular on the rest)

--- src/Twig/Variable/HtmlClass.php
+++ src/Twig/Variable/HtmlClass.php
@@ -19,9 +19,11 @@ class HtmlClass
         $routeParts = explode('_', $this->request->attributes->get('_route'));
 
         $htmlClass = [
-            sprintf('%s-%s', $routeParts[0], $routeParts[1]),
             implode('-', $routeParts)
         ];
+        if (count($routeParts) > 1) {
+            $htmlClass[] = sprintf('%s-%s', $routeParts[0], $routeParts[1]);
+        }
         if (count($routeParts) > 2) {
             $htmlClass[] = "action-{$routeParts[2]}";
         }
