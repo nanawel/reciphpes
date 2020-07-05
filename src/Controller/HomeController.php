20e99e27879ec1ce3a344c94abab7dded46f4a86
nanawel <nanawel@gmail.com>
2020-07-05 10:54:50 +0200

    Add latest added recipes on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+
 class HomeController extends AbstractController
 {
-    public function index()
-    {
-        return $this->render('home.html.twig');
+    public function index() {
+        return $this->render(
+            'home.html.twig',
+            [
+                'recipeCount' => $this->getRecipeCount(),
+                'latestRecipes' => $this->getLatestRecipes()
+            ]
+        );
+    }
+
+    /**
+     * @return int
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    protected function getRecipeCount() {
+        return $this->getEntityManager()->createQueryBuilder()
+            ->from(Recipe::class, 'r')
+            ->select('count(r.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    /**
+     * @return Recipe[]
+     */
+    protected function getLatestRecipes() {
+        return $this->getEntityManager()->getRepository(Recipe::class)
+            ->findBy([], ['createdAt' => 'desc'], 10);
     }
 }
